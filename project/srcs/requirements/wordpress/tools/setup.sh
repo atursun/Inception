c947ae95b8d7d58ce9d73e3b8a1d3eca0ab8295e
apotursun963 <atursun@student.42istanbul.com.tr>
2026-04-21 15:56:53 +0300

    learning how to use adminer

--- project/srcs/requirements/wordpress/tools/setup.sh
+++ project/srcs/requirements/wordpress/tools/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
+# WordPress'in kurulabilmesi için veritabanının hazır olması şarttır. MariaDB (veritabanı) konteyneri bazen WordPress'ten daha yavaş açılabilir. Bu döngü sürekli veritabanına "Hazır mısın?" diye sorar. MariaDB "Hazırım" yanıtı verene kadar WordPress kurulumunu bekletir (Böylece sistemin çökmesini engeller).
 echo ">>> MariaDB bekleniyor..."
 until mysqladmin ping -h"mariadb" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --silent 2>/dev/null; do
     sleep 2
 done
 echo ">>> MariaDB hazır!"
-    
-# wp-config.php kontrolünü doğru path'te yap
+
 if [ ! -f /var/www/html/wp-config.php ]; then
     echo ">>> WordPress yapılandırılıyor..."
 
